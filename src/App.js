7afa85597c25627f6c1ad6641b474cd9fec39bdd
bdgrsrn <bdgrsrn@gmail.com>
2020-05-27 10:42:25 +0800

    i18n and first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { useTranslation } from 'react-i18next';
+
 function App() {
+
+  const { t, i18n } = useTranslation();
+
+  function handleClick(lang){
+    i18n.changeLanguage(lang);
+  }
+
   return (
     <div className="App">
+    <nav style={{width:'100%',padding: '2rem 0', backgroundColor:'gray'}}>
+    <button onClick={()=>handleClick('en')}>
+    English
+    </button>
+    <button onClick={()=>handleClick('ko')}>
+    Korean
+    </button>
+    <button onClick={()=>handleClick('chi')}>
+    China
+    </button>
+    </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {t('Thanks.1')}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {t('Why.1')}
+        </p>
+      
       </header>
     </div>
   );
